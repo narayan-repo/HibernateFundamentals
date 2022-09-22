b7c0c41df5ebfb7af0bec64c6f16f9fb9fc3e455
Narayan Maity <nero.personalrepo@gmail.com>
2022-09-22 21:37:24 +0530

    Add Passenger persistent object
    Use @Table and @SecondaryTable annotation to auto create the tables, and
    the foreign key relation between the PASSENGERS and ADDRESSES table in the database.

--- src/main/java/com/learning/hibernatefundamentals/airport/Main.java
+++ src/main/java/com/learning/hibernatefundamentals/airport/Main.java
@@ -1,7 +1,19 @@
 package com.learning.hibernatefundamentals.airport;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("airport");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Passenger passenger = new Passenger(1, "Narayan Maity", "Medinipur");
+        em.persist(passenger);
+
+        em.getTransaction().commit();
     }
 }
