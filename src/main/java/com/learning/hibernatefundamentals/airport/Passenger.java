e3f943e112f3fb48643e0ee5fad9add8c8975224
Narayan Maity <nero.personalrepo@gmail.com>
2022-09-26 20:42:16 +0530

    Committing files changed after merge.

--- src/main/java/com/learning/hibernatefundamentals/airport/Passenger.java
+++ src/main/java/com/learning/hibernatefundamentals/airport/Passenger.java
@@ -1,34 +1,70 @@
 package com.learning.hibernatefundamentals.airport;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Entity
 @Table(name = "PASSENGERS")
+@SecondaryTable(name = "ADDRESSES",
+        pkJoinColumns = @PrimaryKeyJoinColumn(name = "PASSENGER_ID", referencedColumnName = "PASSENGER_ID"))
 public class Passenger {
 
     @Id
-    @Column(name = "ID")
+    @Column(name = "PASSENGER_ID")
     private int id;
 
-    @Column(name = "NAME")
+    @Column(name = "PASSENGER_NAME", table = "PASSENGERS")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "AIRPORT_ID")
-    private Airport airport;
+    @Column(name = "STREET", table = "ADDRESSES", columnDefinition = "varchar(25) not null")
+    private String street;
 
-    @OneToMany(mappedBy = "passenger")
-    private List<Ticket> tickets = new ArrayList<>();
+    @Column(name = "NUMBER", table = "ADDRESSES", columnDefinition = "varchar(10) not null")
+    private String number;
 
-    public Passenger() {
-    }
+    @Column(name = "ZIP_CODE", table = "ADDRESSES", columnDefinition = "varchar(10) not null")
+    private String zipCode;
+
+    @Column(name = "CITY", table = "ADDRESSES", columnDefinition = "varchar(25) not null")
+    private String city;
 
     public Passenger(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Passenger() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 
     public int getId() {
@@ -45,21 +81,5 @@ public class Passenger {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Airport getAirport() {
-        return airport;
-    }
-
-    public void setAirport(Airport airport) {
-        this.airport = airport;
-    }
-
-    public List<Ticket> getTickets() {
-        return Collections.unmodifiableList(tickets);
-    }
-
-    public void addTicket(Ticket ticket) {
-        tickets.add(ticket);
     }
 }
