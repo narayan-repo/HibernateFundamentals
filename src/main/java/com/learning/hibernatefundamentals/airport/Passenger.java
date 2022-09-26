e7b61bef17367a799ccd36f84c60efbe88445aef
Narayan Maity <nero.personalrepo@gmail.com>
2022-09-26 20:33:14 +0530

    Revert "Merge branch 'secondary_table_multi_fields' with master"
    This reverts commit 69dc108d871ef1a3f50fbedf4be70e25193a2b07, reversing
    changes made to f4cc90df1aee4e04828160c95ab2a2357356e472.

--- src/main/java/com/learning/hibernatefundamentals/airport/Passenger.java
+++ src/main/java/com/learning/hibernatefundamentals/airport/Passenger.java
@@ -1,70 +1,34 @@
 package com.learning.hibernatefundamentals.airport;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "PASSENGERS")
-@SecondaryTable(name = "ADDRESSES",
-        pkJoinColumns = @PrimaryKeyJoinColumn(name = "PASSENGER_ID", referencedColumnName = "PASSENGER_ID"))
 public class Passenger {
 
     @Id
-    @Column(name = "PASSENGER_ID")
+    @Column(name = "ID")
     private int id;
 
-    @Column(name = "PASSENGER_NAME", table = "PASSENGERS")
+    @Column(name = "NAME")
     private String name;
 
-    @Column(name = "STREET", table = "ADDRESSES", columnDefinition = "varchar(25) not null")
-    private String street;
+    @ManyToOne
+    @JoinColumn(name = "AIRPORT_ID")
+    private Airport airport;
 
-    @Column(name = "NUMBER", table = "ADDRESSES", columnDefinition = "varchar(10) not null")
-    private String number;
-
-    @Column(name = "ZIP_CODE", table = "ADDRESSES", columnDefinition = "varchar(10) not null")
-    private String zipCode;
-
-    @Column(name = "CITY", table = "ADDRESSES", columnDefinition = "varchar(25) not null")
-    private String city;
-
-    public Passenger(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @OneToMany(mappedBy = "passenger")
+    private List<Ticket> tickets = new ArrayList<>();
 
     public Passenger() {
     }
 
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
+    public Passenger(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public int getId() {
@@ -81,5 +45,21 @@ public class Passenger {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Airport getAirport() {
+        return airport;
+    }
+
+    public void setAirport(Airport airport) {
+        this.airport = airport;
+    }
+
+    public List<Ticket> getTickets() {
+        return Collections.unmodifiableList(tickets);
+    }
+
+    public void addTicket(Ticket ticket) {
+        tickets.add(ticket);
     }
 }
